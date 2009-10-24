74e78ef9e0b4ce2ccb33271181e99dbb2393d39a
Andrius Chamentauskas <sinsiliux@gmail.com>
2009-10-24 14:39:21 +0300

    Prideta daugiau duomenu

--- db/blueprint.rb
+++ db/blueprint.rb
@@ -5,12 +5,15 @@ end
 
 blueprint :projects => :users do
   @first_project = @admin.leaded_projects.blueprint(:name => 'first')
+  @jonas_project = @jonas.leaded_projects.blueprint(:name => 'jonas project')
 end
 
 blueprint :budgets => :projects do
   @first_project_0910 = @first_project.budgets.blueprint(:at => Time.mktime(2009, 10), :hours => 130)
   @first_project_0911 = @first_project.budgets.blueprint(:at => Time.mktime(2009, 11), :hours => 142)
   @first_project_0912 = @first_project.budgets.blueprint(:at => Time.mktime(2009, 12), :hours => 128)
+
+  @jonas_project_0912 = @jonas_project.budgets.blueprint(:at => Time.mktime(2009, 12), :hours => 54)
 end
 
 blueprint :tasks => [:budgets, :users] do
@@ -21,4 +24,6 @@ blueprint :tasks => [:budgets, :users] do
   @jonas_first_project_0910 = @first_project_0910.tasks.blueprint(:work_hours => 68, :user => @jonas)
   @jonas_first_project_0911 = @first_project_0911.tasks.blueprint(:work_hours => 61, :user => @jonas)
   @jonas_first_project_0912 = @first_project_0912.tasks.blueprint(:work_hours => 60, :user => @jonas)
+
+  @jonas_first_project_0912 = @jonas_project_0912.tasks.blueprint(:work_hours => 55, :user => @jonas)
 end
