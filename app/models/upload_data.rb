54af8c4ca668c7b4bbdc812d5f891c12bd54a7c7
Povilas Liubauskas <liubauskas@gmail.com>
2009-11-03 13:52:50 +0200

    Fixed upload_data model (projects now have leaders)

--- app/models/upload_data.rb
+++ app/models/upload_data.rb
@@ -19,20 +19,18 @@ class UploadData
     end
 
     FasterCSV.foreach(csv_file(3)) do |dep_row|
-      department_last = Department.new do |depart|
+      Department.create do |depart|
         depart.name = dep_row[0]
       end
-      department_last.save
       department_last = Department.last
       FasterCSV.foreach(csv_file(2)) do |user_row|
         if user_row[1] == dep_row[0]
-          user = User.new do |user|
+          user = User.create do |user|
             user.name = user_row[0]
-            user.email = "#{user_row[0]}@system.local"
-            user.password = rand(0).to_s
+            user.email = "#{rand(0)}@localhost.com"
+            user.password = user.password_confirmation = rand(0).to_s
             user.department = department_last
           end
-          user.save_with_validation(false)
           if user_row[0] == dep_row[1]
             department_last.leader = user
             department_last.save
@@ -42,28 +40,26 @@ class UploadData
     end
 
     FasterCSV.foreach(csv_file(1)) do |proj_row|
-      project_last = Project.new do |project|
+      Project.create do |project|
         project.name = proj_row[0]
+        project.leader = User.find_by_name(proj_row[1])
       end
-      project_last.save
       project_last = Project.last
       FasterCSV.foreach(csv_file(4)) do |bud_row|
         if bud_row[0] == proj_row[0]
-          budget_last = Budget.new do |budget|
+          Budget.create do |budget|
             budget.at = Date.new(bud_row[1].to_i, bud_row[2].to_i)
             budget.hours = bud_row[3].to_i
             budget.project = project_last
           end
-          budget_last.save
           budget_last = Budget.last
           FasterCSV.foreach(csv_file(0)) do |task_row|
             if task_row[0] == bud_row[0] && task_row[2] == bud_row[1] && task_row[3] == bud_row[2]
-              task = Task.new do |task|
+              Task.create do |task|
                 task.work_hours = task_row[4].to_i
                 task.budget = budget_last
-                task.user = User.first(:conditions => { :name => task_row[1]})
+                task.user = User.find_by_name(task_row[1])
               end
-              task.save
             end
           end
         end
