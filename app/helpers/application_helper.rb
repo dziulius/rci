d51dde2d3b6b374c6fac6c03e87dbbb86655db29
Andrius Chamentauskas <sinsiliux@gmail.com>
2009-11-05 20:26:53 +0200

    Enhanced field_list_item to take any number of arguments and return nil if any of them returns nil

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,11 @@ module ApplicationHelper
     content_for(:title, text)
   end
 
-  def field_list_item(object, field, method = nil)
-    value = object.send(field)
-    value = value.send(method) if method
+  def field_list_item(object, *fields)
+    value = fields.inject(object) {|val, field| val.try(field) }
 
     content_tag('li') do
-      content_tag('span', "#{object.class.human_attribute_name(field)}", :class => 'header') + value.to_s
-    end
+      content_tag('span', "#{object.class.human_attribute_name(fields[-2] || fields[-1])}", :class => 'header') + value.to_s
+    end if value
   end
 end
