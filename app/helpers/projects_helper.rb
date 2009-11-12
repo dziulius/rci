0bbafdd85fbe4da7872791facfbf52027850e29d
Andrius Chamentauskas <sinsiliux@gmail.com>
2009-11-12 20:23:33 +0200

    Implemented showing users of project together with how much hours they spent. See #18

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,17 @@
 module ProjectsHelper
+  def hours_worked(project)
+    real_work = project.users.sum(&:work_hours)
+    budget = project.budgets.sum('hours')
+    
+    color = if real_work < budget
+      'orange'
+    elsif real_work > budget
+      'red'
+    else
+      'green'
+    end
+
+    content_tag('p', "Total hours worked: <span class='#{color}'>#{real_work}</span>/#{budget} (<span class='#{color}'>#{
+            real_work * 100 / budget}%</span>)")
+  end
 end
--- spec/helpers/projects_helper_spec.rb
+++ spec/helpers/projects_helper_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 
 describe ProjectsHelper do
+  describe "hours spent" do
+    it "should be shown in orange if project is under budget" do
+      build :tasks
+      helper.hours_worked(@psi).should have_tag('p', :text => 'Total hours worked: 404/411 (98%)') do
+        with_tag('span.orange', :text => '404')
+        with_tag('span.orange', :text => '98%')
+      end
+    end
 
-  #Delete this example and add some real ones or delete this file
-  it "should be included in the object returned by #helper" do
-    included_modules = (class << helper; self; end).send :included_modules
-    included_modules.should include(ProjectsHelper)
+    it "should be shown in green if project is exactly on budget" do
+      build :tasks
+      @andrius.tasks.create!(:budget => @budgets_of_psi_at_1002, :work_hours => 7)
+      helper.hours_worked(@psi).should have_tag('p', :text => 'Total hours worked: 411/411 (100%)') do
+        with_tag('span.green', :text => '411')
+        with_tag('span.green', :text => '100%')
+      end
+    end
+
+    it "should be shown in red if project is over budget" do
+      build :tasks
+      @andrius.tasks.create!(:budget => @budgets_of_psi_at_1002, :work_hours => 12)
+      helper.hours_worked(@psi).should have_tag('p', :text => 'Total hours worked: 416/411 (101%)') do
+        with_tag('span.red', :text => '416')
+        with_tag('span.red', :text => '101%')
+      end
+    end
   end
-
 end
