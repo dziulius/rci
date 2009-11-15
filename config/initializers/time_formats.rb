6ca7d0ebbf26059797ecfff5eea5fbc00a0b82d7
Andrius Chamentauskas <sinsiliux@gmail.com>
2009-11-15 20:42:16 +0200

    Extracted date conversions. See #20

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,2 @@
+Date::DATE_FORMATS[:short_year_month] = "%Y/%m"
+Date::DATE_FORMATS[:long_year_month] = "%Y %B"
--- spec/helpers/dates_helper_spec.rb
+++ spec/helpers/dates_helper_spec.rb
@@ -3,10 +3,10 @@ require File.dirname(__FILE__) + "/../spec_helper"
 describe DatesHelper do
   it "should return correct combo box for date selection" do
     helper.compact_month_select(:date, Date.new(2009, 11), Date.new(2009, 10), Date.new(2010, 1)).should have_tag('select#date[name=date]') do
-      with_tag 'option[value=?]', '2009/10', :text => '2009-October'
-      with_tag 'option[value=?][selected=selected]', '2009/11', :text => '2009-November'
-      with_tag 'option[value=?]', '2009/12', :text => '2009-December'
-      with_tag 'option[value=?]', '2010/01', :text => '2010-January'
+      with_tag 'option[value=?]', '2009/10', :text => '2009 October'
+      with_tag 'option[value=?][selected=selected]', '2009/11', :text => '2009 November'
+      with_tag 'option[value=?]', '2009/12', :text => '2009 December'
+      with_tag 'option[value=?]', '2010/01', :text => '2010 January'
     end
   end
 
