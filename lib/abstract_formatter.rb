39d647188a9eb2462903c1800c0ddf8d1d810119
Andrius Chamentauskas <sinsiliux@gmail.com>
2009-11-28 17:32:25 +0200

    Fixed warning about constant already initialized for formatters. See #31

--- lib/abstract_formatter.rb
+++ lib/abstract_formatter.rb
@@ -1,7 +1,7 @@
 module Formatters
   class AbstractFormatter
     attr_reader :helper, :column, :args
-    
+
     def initialize(helper, column, *args)
       @column = column
       @helper = helper
@@ -17,11 +17,13 @@ module Formatters
     module ClassMethods
       def formatter(name, &block)
         class_name = name.to_s.classify + 'Formatter'
-        Formatters.const_set(class_name, Class.new(AbstractFormatter) do
-          define_method(:to_s) do |subject|
-            block.call(subject, self.helper, self.column, *self.args).to_s
-          end
-        end)
+        unless Formatters.constants.include?(class_name)
+          Formatters.const_set(class_name, Class.new(AbstractFormatter) do
+            define_method(:to_s) do |subject|
+              block.call(subject, self.helper, self.column, *self.args).to_s
+            end
+          end)
+        end
 
         define_method(name) do |*args|
           Formatters.const_get(class_name).new(self, *args)
