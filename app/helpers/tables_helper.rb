aaa86ac14e9f0dba0227e8b92482fa02e712457b
Andrius Chamentauskas <sinsiliux@gmail.com>
2009-12-03 18:14:48 +0200

    Added ability for content row to be wrapped in form, refactored tables helper. See #15

--- app/helpers/tables_helper.rb
+++ app/helpers/tables_helper.rb
@@ -1,7 +1,13 @@
 module TablesHelper
   include FormattingHelper
 
-  def table_for(klass, list, *columns)
+  def table_for(klass, list, *columns, &block)
+    options = columns.extract_options!
+    if with_form = options.delete(:with_form)
+      with_form = [] if with_form == true
+      with_form = [*with_form]
+    end
+
     concat(content_tag('table') do
       content = ''
       content << content_tag('tr') do
@@ -11,14 +17,8 @@ module TablesHelper
       end
 
       content << list.collect do |item|
-        content_tag('tr') do
-          tds = if block_given?
-            yield(item)
-          else
-            columns.collect { |column| column_value_for(item, column) }
-          end
-          tds.collect {|value| content_tag 'td', value }
-        end
+        row = content_row(row_values_for(item, columns, &block))
+        with_form ? semantic_form_for(with_form + [item]) { concat(row) } : row
       end.join
 
       content << @content_for_table_bottom.to_s
@@ -29,25 +29,32 @@ module TablesHelper
 
   def table_with_totals_for(klass, list, *columns, &block)
     content_for :table_bottom do
-      content_tag 'tr' do
-        object = ARMock.new list
-        values = if block_given?
-          yield(object)
-        else
-          [nil, *columns[1..-1].collect { |column| column_value_for(object, column) }]
-        end
-        values[0] = t('common.total')
-
-        content_tag('th', values.shift) + values.collect do |value|
-          content_tag('td', value)
-        end.join
-      end
+      tds = row_values_for(ARMock.new(list), columns, 1, t('common.total'), &block)
+      content_row(tds, :first_header => true)
     end
 
     table_for(klass, list, *columns, &block)
   end
 
   protected
+
+  def row_values_for(object, columns, start_with = 0, empty_value = nil)
+    empty = [empty_value] * start_with
+    if block_given?
+      empty + yield(object)[start_with..-1]
+    else
+      empty + columns[start_with..-1].collect { |column| column_value_for(object, column) }
+    end
+  end
+
+  def content_row(values, options = {})
+    content_tag('tr') do
+      ''.tap do |result|
+        result << content_tag('th', values.shift) if options[:first_header]
+        result << values.collect { |value| content_tag('td', value) }.join
+      end
+    end
+  end
 
   def column_value_for(object, column)
     column.is_a?(Symbol) ? object.send(column) : column.to_s(object)
