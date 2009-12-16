eca1607f5e9c03dfe511c65dc960e58b88ad2a16
Andrius Chamentauskas <sinsiliux@gmail.com>
2009-12-16 13:53:34 +0200

    Added user project chart

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,3 +3,26 @@ function tabs(spinner_label) {
         $('#tabs').tabs({ cache: true, spinner: spinner_label });
     });
 }
+
+function collectText(elements) {
+    var points = [];
+    $(elements).each(function(n, element) {
+        points.push([n, $(element).text()]);
+    });
+    return points;
+}
+
+function collectInt(elements) {
+    var points = [];
+    $(elements).each(function(n, element) {
+        points.push([n, parseInt($(element).text())]);
+    });
+    return points;
+}
+
+function reverse(points) {
+    $.each(points, function(n, point) {
+        points[n] = [point[1], point[0]];
+    });
+    return points;
+}
